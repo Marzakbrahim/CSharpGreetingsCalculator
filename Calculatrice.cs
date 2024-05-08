331d15d0281b64204fb539fadb98d017842b752e
Marzakbrahim <marzakbrahim15@gmail.com>
2024-05-08 17:23:13 +0200

    code update

---
+++ Calculatrice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Calculatrice
+{
+	public double Addition(double a , double b)
+	{
+		return a+b ;
+	}
+
+	public double Soustraction(double a , double b)
+		{
+			return a-b ;
+		}
+	public double Multiplication(double a , double b)
+	{
+		return a*b ;
+	}
+	public double Division(double a , double b)
+	{
+        return a / b;
+
+    }
+	public void Quitter()
+	{
+        Console.WriteLine("Vous avez quitté");
+        
+
+    } 
+
+}
--- Program.cs
+++ Program.cs
@@ -1,187 +1,138 @@
-﻿/*
-using System;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Formats.Asn1;
 
-class Program
+namespace calculatrice
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Bienvenue !");
-        double moyenne = CalculerMoyenne();
-        Console.WriteLine("La moyenne est : " + moyenne);
+        private static string choose = string.Empty ;
+        static void Main(string[] args)
+        {
+            try
+            {
+                mainApplication();
+            }
+            catch (Exception e )
+            {
+
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+
+
+        }
+
+        private static void mainApplication()
+        {
+            double a = 0; // 1er nombre
+            double b = 0; // 2eme nombre
+            String c = string.Empty;  // l'opération
+            double resultat = 0;
+            bool saisieValide = false;
+
+
+            while (saisieValide == false)
+            {
+                //////////////////////////////////////////////////
+                Console.WriteLine("Entrer le 1er nombre :");
+                if (!double.TryParse(Console.ReadLine(), out a))
+                {
+                    Console.WriteLine("Vous n'avez pas entré un entier ! veuillez saisir un nombre entier svp");
+                }
+                else
+                {
+                    saisieValide = true;
+                }
+            }
+            saisieValide = false;
+            while (saisieValide == false)
+            {
+                Console.WriteLine("Entrer le 2er nombre :");
+                if (!double.TryParse(Console.ReadLine(), out b))
+                {
+                    Console.WriteLine("Vous n'avez pas entré un entier ! veuillez saisir un nombre entier svp");
+                }
+                else
+                {
+                    saisieValide = true;
+                }
+            }
+
+            saisieValide = false;
+            while (saisieValide == false)
+            {
+                Console.WriteLine("Entrez une opération (+ ou - ou * ou /) ou 'Q' pour quitter");
+                c = Console.ReadLine();
+                if (c == "+" || c == "*" || c == "-" || c == "/" || c == "Q")
+                {
+                    saisieValide = true;
+                }
+                else
+                {
+                    Console.WriteLine("Votre saisie est invalide !");
+                }
+            }
+
+
+            Calculatrice calculatrice = new Calculatrice();
+            switch (c)
+            {
+                case "+":
+                    resultat = calculatrice.Addition(a, b);
+                    Console.WriteLine("le résultat est : " + resultat);
+                    menu();
+                    break;
+                case "-":
+                    resultat = calculatrice.Soustraction(a, b);
+                    Console.WriteLine("le résultat est : " + resultat);
+                    menu();
+                    break;
+                case "*":
+                    resultat = calculatrice.Multiplication(a, b);
+                    Console.WriteLine("le résultat est : " + resultat);
+                    menu();
+                    break;
+                case "/":
+                    try
+                    {
+                        resultat = calculatrice.Division(a, b);
+                        Console.WriteLine("le résultat est : " + resultat);
+                        menu();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                    break;
+                case "Q":
+
+                    calculatrice.Quitter();
+                    break;
+            }
+            Console.ReadKey();
+        }
+
+        // Application menu pour gérer l'arrêt : 
+        private static void menu()
+        {
+            while(choose != "Q")
+            {
+                Console.WriteLine("Tapez R pour refaire une opération ou Q pour quitter l'application.");
+                choose = Console.ReadLine();
+                switch (choose)
+                {
+                    case "R":
+                        mainApplication();
+                        break;
+                    case "Q":
+                        Console.WriteLine("Vous avez Quitter l'application au revoir !");
+                        break;
+                    default: Console.WriteLine("Votre saisie est invalide !");
+                        break;
+                }
+            }
+        }
     }
-
-    static double CalculerMoyenne()
-    {
-    double num1 = 0, num2 = 0, num3 = 0;
-    bool saisieValide = false;
-
-    while (!saisieValide)
-    {
-        Console.WriteLine("Entrez le premier nombre :");
-        if (!double.TryParse(Console.ReadLine(), out num1))
-        {
-            Console.WriteLine("Vous n'avez pas entré un nombre valide. Veuillez réessayer.");
-        }
-        else
-        {
-            saisieValide = true;
-        }
-    }
-
-    saisieValide = false;
-    while (!saisieValide)
-    {
-        Console.WriteLine("Entrez le deuxième nombre :");
-        if (!double.TryParse(Console.ReadLine(), out num2))
-        {
-            Console.WriteLine("Vous n'avez pas entré un nombre valide. Veuillez réessayer.");
-        }
-        else
-        {
-            saisieValide = true;
-        }
-    }
-
-    saisieValide = false;
-    while (!saisieValide)
-    {
-        Console.WriteLine("Entrez le troisième nombre :");
-        if (!double.TryParse(Console.ReadLine(), out num3))
-        {
-            Console.WriteLine("Vous n'avez pas entré un nombre valide. Veuillez réessayer.");
-        }
-        else
-        {
-            saisieValide = true;
-        }
-    }
-
-    double moyenne = (num1 + num2 + num3) / 3;
-    return moyenne;
-    }
-
 }
-*/
-using System ;
-class Program 
-{
-    static void Main(String[] args)
-    {
-        //String nom ;
-        //Console.WriteLine("Entrez votre nom svt !");
-        //nom=Console.ReadLine();
-        //Console.WriteLine("Salut {0}! bienvenue dans notre programme :",nom);
-        DateTime dateCourante=DateTime.Now;
-        if (dateCourante.DayOfWeek != DayOfWeek.Saturday && dateCourante.DayOfWeek != DayOfWeek.Sunday && (dateCourante.DayOfWeek != DayOfWeek.Friday || dateCourante.Hour <= 18))
-        {
-            // on est en semaine 
-            if (dateCourante.Hour > 6 && dateCourante.Hour < 18)
-            {
-                // on est le jour
-                afficherBonjour();
-            }
-            else
-            {
-                // on est le soir 
-                afficherBonsoir();
-            }
-        }
-        else
-        {
-            // on est le weekend
-            afficherBonWeekEnd();
-        }
-        int somme=calculeroperation();
-        Console.WriteLine("le résultat est : {0}",somme);
-
-    }
-    static void afficherBonjour(){
-        Console.WriteLine("Bonjour " + Environment.UserName);
-    }
-
-    static void afficherBonsoir(){
-        Console.WriteLine("Bonsoir " + Environment.UserName);
-    }
-
-    static void afficherBonWeekEnd(){
-        Console.WriteLine("Bon Week-end " + Environment.UserName);
-    }
-    static int calculeroperation()
-    {
-        int a=0 ;
-        int b=0;
-        char op=' ';
-        bool saisieValide= false;
-        while(!saisieValide)
-        {
-            Console.WriteLine("Entrer le 1er nombre :");
-            if(!int.TryParse(Console.ReadLine(),out a))
-            {
-                Console.WriteLine("Vous n'avez pas entré un entier ! veuillez saisir un nombre entier svp");
-            }
-            else 
-            {
-                saisieValide= true;
-            }
-        }
-
-
-
-        saisieValide= false;
-        while(!saisieValide)
-        {
-            Console.WriteLine("Entrer le 2emme nombre :");
-            if(!int.TryParse(Console.ReadLine(),out b))
-            {
-                Console.WriteLine("Vous n'avez pas entré un entier ! veuillez saisir un nombre entier svp");
-            }
-            else 
-            {
-                saisieValide= true;
-            }
-        }
-
-
-        saisieValide= false;
-        while(!saisieValide)
-        {
-            Console.WriteLine("Entrer une opération (*,+,-,/):");
-            op=char.Parse(Console.ReadLine());
-            if(op !='*' && op !='-' && op !='+' && op !='/')
-            {
-                Console.WriteLine("L\'opération est invalide ! entrez une opération valide svp !");
-            }
-            else 
-            {
-                saisieValide= true;
-            }
-        }
-        int result=0;
-        switch(op)
-        {
-            case '+':
-            {
-                result=a+b;
-                break;
-            }
-            case '-':
-            {
-                result=a-b;
-                break;
-            }
-            case '*':
-            {
-                result=a*b;
-                break;
-            }
-            case '/':
-            {
-                result=a/b;
-                break;
-            }
-        }
-        return result;
-    }
-
-}
-
